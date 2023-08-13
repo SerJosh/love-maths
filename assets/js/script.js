0111724d860308569bce28e7c6c5163eac2f35a9
SerJosh <>
2023-08-13 13:37:42 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function chechAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
